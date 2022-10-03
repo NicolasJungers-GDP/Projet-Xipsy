1350db1bea68412a3267a816d809fe591a106146
Wouchiii <nathanbellaiche000@gmail.com>
2022-10-03 17:13:47 +0200

    dfghjk

--- Source/OrbitalJourney/OrbitalJourney.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "OrbitalJourney.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OrbitalJourney, "OrbitalJourney" );
- 
